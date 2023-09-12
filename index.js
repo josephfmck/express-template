8b6c146718921e94517cb08450a732de033ea47a
Joe <josephfmck@gmail.com>
2023-09-12 15:28:32 -0500

    middleware logger, logs out the req properties of the url

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const app = express();
 //middleware - handles req, res (next moves to next middleware in step)
 //? everytime we make a request, this runs. Ex. req to api/members, this logs in console
 const logger = (req, res, next) => {
-    console.log('hello');
+    //? protocol - http or https, get() = parts of url, host - localhost:5000, originalURL - /api/members page
+    console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}`);
     next();
 };
 
